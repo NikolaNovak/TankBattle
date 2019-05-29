08dd271589484281cd2c030f46fdcca4fd022ced
Nikola Novak <nikolan97@live.com>
2019-05-30 00:39:10 +0200

    Added Tick & AimTowardsCrosshair methods

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -17,6 +17,7 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+public:
 	virtual void BeginPlay() override;
 
 private:
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -16,7 +16,12 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
 	ATank* GetControlledTank() const;
 
-	virtual void BeginPlay() override;
+	//Start the tank moving the barrel so that a shot would hit 
+	//where the corsshair intersects the world
+	void AimTowardsCrosshair();
 };
