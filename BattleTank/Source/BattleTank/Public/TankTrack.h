4841933079440b4f5793a8d3f6d6aab63b0ed249
Nikola Novak <nikolan97@live.com>
2019-06-07 01:02:00 +0200

    The tank tracks can now be controlled to move the tank forward/backward or rotate

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -15,6 +15,11 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 public:
+	//Sets a throttle between -1 and +1
 	UFUNCTION(Blueprintcallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	// Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume 40t tank, and 1g acc
 };
