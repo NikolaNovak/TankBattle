69833e197e7c65c903629af4dd42e44379377664
Nikola Novak <nikolan97@live.com>
2019-06-21 02:04:04 +0200

    The tanks now move more naturally

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -14,12 +14,30 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
+private:
+
+	UTankTrack();
+
+	virtual void BeginPlay() override;
+
+	void ApplySidewaysForce();
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	float CurrentThrottle = 0;
+
 public:
+
 	//Sets a throttle between -1 and +1
 	UFUNCTION(Blueprintcallable, Category = "Input")
 	void SetThrottle(float Throttle);
 
+	void DriveTrack();
+
 	// Max force per track in Newtons
 	UPROPERTY(EditDefaultsOnly)
-	float TrackMaxDrivingForce = 400000; // Assume 40t tank, and 1g acc
+	float TrackMaxDrivingForce = 5000000;
+	// Assume 40t tank, and 1g acc
+
 };
