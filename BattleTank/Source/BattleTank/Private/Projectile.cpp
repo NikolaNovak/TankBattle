9f66a37e988121c5aa086bc236ab76517ca8c939
Nikola Novak <nikolan97@live.com>
2019-07-03 00:33:03 +0200

    Preparing for particles on the projectile

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Projectile.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "Engine/World.h"
 
 // Sets default values
@@ -9,6 +12,14 @@ AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision mesh"));
+	SetRootComponent(CollisionMesh);
+	CollisionMesh->SetNotifyRigidBodyCollision(true);
+	CollisionMesh->SetVisibility(false);
+
+	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
+	LaunchBlast->SetupAttachment(CollisionMesh);
 
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile movement component"));
 	ProjectileMovement->bAutoActivate = false;
